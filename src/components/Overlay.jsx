677ce1b98b557b6cac3f35d90216ce9186878396
Raphael Mayer <raphael_mayer@hotmail.de>
2018-12-07 16:20:32 +0100

    added repo link to projects

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -9,18 +9,19 @@ const Overlay = (props) => {
         	<div className="overlay" style={ visible ? { transform: "translateX(0)", opacity: 1, visibility: "visible" } : null }>
                 <NavBarIcon onClick={ props.handleOverlay } transform={ true } />
         		<div className="overlay-text-box">
-                    <h2>{ p.title }</h2>
+                    <h2>{ p.title } {p.repo && <a href={ p.repo } target="_blank" rel="noopener noreferrer"></a>}</h2>
                     <p><em>{ p.tagline }</em></p>
                     <h3>Main Features</h3>
                     <p>{ p.features && p.features.map((f, i) => <li key={ i }> { f } </li>) }</p>
                     <h3>Details</h3>
                     <p>{ p.description }</p>
                     <a href={ p.link } target="_blank" rel="noopener noreferrer"><button className="overlay-btn">View live</button></a>
+                    <a href={ p.repo } target="_blank" rel="noopener noreferrer"><button className="overlay-btn">View repository</button></a>
                 </div>
                 <div className="overlay-img-box">
                     {
                         p.images &&        // dont render preview image ([0])
-                        p.images.map((img, i) => i === 0 ? null : <img className="overlay-img" alt="p-img" src={ `${process.env.PUBLIC_URL}/images/${img}` } />)
+                        p.images.map((img, i) => /* i === 0 ? null : */ <img className="overlay-img" alt="p-img" src={ `${process.env.PUBLIC_URL}/images/${img}` } />)
                     }
                 </div>
             </div>
