b5d024144259a18edfb02e7a5e9027d0cfa44aee
Raphael Mayer <raphael_mayer@hotmail.de>
2019-01-27 17:21:56 +0100

    Updates

---
+++ precache-manifest.9e89f45a79570b7d180370d03517e892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f171de5904aca44e950",
+    "url": "/go/static/js/runtime~main.1f171de5.js"
+  },
+  {
+    "revision": "0c12bdde6dedee3836f6",
+    "url": "/go/static/js/main.0c12bdde.chunk.js"
+  },
+  {
+    "revision": "0a8772e40fa3157aa51f",
+    "url": "/go/static/js/1.0a8772e4.chunk.js"
+  },
+  {
+    "revision": "0c12bdde6dedee3836f6",
+    "url": "/go/static/css/main.8c51196f.chunk.css"
+  },
+  {
+    "revision": "0cbca19d77564bda7deed0f4288ad327",
+    "url": "/go/index.html"
+  }
+];
