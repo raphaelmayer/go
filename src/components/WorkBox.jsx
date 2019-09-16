9473476067e3eed22e2cb12606262bbdf763514d
Raphael Mayer <35350266+raphaelmayer@users.noreply.github.com>
2019-09-16 23:50:26 +0200

    darkmode setting workbox

--- src/components/WorkBox.jsx
+++ src/components/WorkBox.jsx
@@ -5,26 +5,9 @@ const WorkBox = ({ p, i, onClick }) => {
     return(
         <div className="workbox" onClick={ e => onClick(p) } >
             <img className="workbox-img" alt="" src={`${process.env.PUBLIC_URL}/images/${p.thumbnail}`} />
-            <p className="workbox-title" style={{ background: `linear-gradient(to right, ${makeBlue(i)}, ${makeBlue(i+2)})` }} >{ p.title }</p>
+            <p className="workbox-title">{ p.title }</p>
         </div>
     );
 }
 
 export default WorkBox;
-
-const makeBlue = i => {
-	i = i < 10 ? i : i.toString().split("")[1];
-	const arr = [
-		"#112222",
-		"#113333",
-		"#114444",
-		"#115555",
-		"#116666",
-		"#117777",
-		"#115555",
-		"#114444",
-		"#113333",
-		"#112222"
-	];
-	return arr[i] || "#115555";
-}
